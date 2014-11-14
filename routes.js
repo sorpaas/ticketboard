1e230d142479aafddc2b767c1eed530d24a7fc23
sorpa'as plat <sorpaas@gmail.com>
2014-11-14 22:42:35 +0800

    add npm and make request.body work

--- routes.js
+++ routes.js
@@ -43,3 +43,33 @@ Router.onBeforeAction(function() {
     this.next();
   }
 }, { except: [ 'inbound' ] });
+
+if (Meteor.isServer) {
+  var Busboy = Meteor.npmRequire("busboy"),
+      fs = Npm.require("fs"),
+      os = Npm.require("os"),
+      path = Npm.require("path");
+
+  Router.onBeforeAction(function (req, res, next) {
+    var filenames = []; // Store filenames and then pass them to request.
+
+    if (req.method === "POST") {
+      var busboy = new Busboy({ headers: req.headers });
+      busboy.on("file", function (fieldname, file, filename, encoding, mimetype) {
+        var saveTo = path.join(os.tmpDir(), filename);
+        file.pipe(fs.createWriteStream(saveTo));
+        filenames.push(saveTo);
+      });
+      busboy.on("field", function(fieldname, value) {
+        req.body[fieldname] = value;
+      });
+      busboy.on("finish", function () {
+        // Pass filenames to request
+        req.filenames = filenames;
+        next();
+      });
+    }
+    // Pass request to busboy
+    req.pipe(busboy);
+ });
+}
